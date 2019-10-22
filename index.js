e4b9ee8bc1a6a4254d2503e5384f315969b2375f
taku5 <caenorhabditis.elegans516@gmail.com>
2019-10-22 18:59:59 +0900

    寿司とピザのアンケートを追加

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ const server = http.createServer((req, res) => {
           firstItem: 'ごはん',
           secondItem: 'パン'
         }));
+      } else if (req.url === '/enquetes/sushi-pizza') {
+        res.write(pug.renderFile('./form.pug', {
+          path: req.url,
+          firstItem: 'すし',
+          secondItem: 'ピザ'
+        }));
       }
       res.end();
       break;
